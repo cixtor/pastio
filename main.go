6506b6c9b22506103d33e6c9c9296189e4558b27
Cixtor <cixtords@gmail.com>
2020-05-06 09:00:00 -0800

    Convert PublicFolder constant into a program argument

--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 	"github.com/cixtor/middleware"
 )
 
-// PublicFolder is the document root.
-const PublicFolder = "assets"
-
 // StorageFolder is where the submissions will be stored.
 const StorageFolder = "storage"
 
@@ -19,15 +16,17 @@ var app Application
 var router = middleware.New()
 
 var addr string
+var publicHTML string
 
 func main() {
+	flag.StringVar(&publicHTML, "public-html", "assets", "Directory with all the js, css, and image files")
 	flag.StringVar(&addr, "addr", ":3000", "Hostname and port number to listen for HTTP requests")
 	flag.Parse()
 
 	router.ReadTimeout = time.Second * 5
 	router.WriteTimeout = time.Second * 10
 
-	router.STATIC(PublicFolder, "/assets")
+	router.STATIC(publicHTML, "/assets")
 
 	if err := router.ListenAndServe(addr); err != nil {
 		log.Fatal(err)
